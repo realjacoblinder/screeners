311698d4f8a4360fe9f4c7b67d9b2446a71e9dae
Jacob <jl@jacoblinder.net>
2021-03-12 12:00:45 -0500

    added error codes

--- options_return_screener/infinite_tendies.py
+++ options_return_screener/infinite_tendies.py
@@ -20,7 +20,7 @@ start_time = time.time()
 # Import the ticker symbols
 tickas_df = pd.read_csv('https://www.cboe.com/us/options/symboldir/equity_index_options/?download=csv')
 tickas_list_initial = tickas_df[' Stock Symbol'].to_list()
-tickas = [ticka for ticka in tickas_list_initial if len(ticka) < 5]
+tickas = [ticka for ticka in tickas_list_initial if len(ticka) < 6]
 
 # set up files and folders
 if os.path.isfile(r'options_errors.log'):
@@ -31,34 +31,39 @@ if os.path.isdir(r'repo'):
 os.mkdir(r'repo')
 
 #for testing:
-#tickas = ['idex']#tickas[13:27]
+#tickas = ['gme']#tickas[13:27]
 
 '''Conditions
 Solve for any options where the strike is less than spot and the return is >= 50%
 '''
 
-required_return = 0.50
+required_return = 0.30
 spot_modifier = 0.67
 spot_upper_limit = 10
 
 for ticka in tickas:
+    print(ticka)
     today = datetime.today()
-    master_df = pd.DataFrame(columns=['Contract Name', 'Strike', 'Bid', 'Ask', 'Volume', 'Open Interest', 'Return', 'Last Trade Date'])
+    master_df = pd.DataFrame()
     try:
         spot_price = yf_s.get_live_price(ticka)
         
         if spot_price > spot_upper_limit:
+            with open('options_errors.log', 'a') as f:
+                timestamp = datetime.now()
+                f.write(f'{timestamp} - {ticka} spot was too high at {spot_price}\n')
             continue
         
         for exp_date in yf_o.get_expiration_dates(ticka):
             try:
                 data = yf_o.get_puts(ticka, exp_date).replace('-', 0)
                 data = data.drop(['Change', '% Change', 'Implied Volatility'], 1)
-                
                 # check strikes before doing work
                 data['Strike'] = data['Strike'].astype(float)
                 data = data[data['Strike'] <= spot_price*spot_modifier]
                 if data.empty:
+                    with open('options_errors.log', 'a') as f:
+                        f.write(f'{timestamp} - {ticka}, was empty after strike vs spot comparison for {exp_date}\n')
                     continue
                 
                 
@@ -76,7 +81,6 @@ for ticka in tickas:
                 cleaned_df = cleaned_df.reset_index(drop=True)
                 cleaned_df = recent_trades_only(cleaned_df, today)
                 master_df = master_df.append(cleaned_df, ignore_index=True)
-
             except:
                 timestamp = datetime.now()
                 with open('options_errors.log', 'a') as f:
@@ -94,6 +98,7 @@ for ticka in tickas:
         master_df.to_csv(f'repo/{ticka}_infinite tendies_incoming.csv', index=False)
         timestamp = datetime.now()
         with open('options_errors.log', 'a') as f:
+            timestamp = datetime.now()
             f.write(f'{timestamp} - {ticka} has those juicy infinite tenders\n')        
      
     else:
